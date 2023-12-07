9c80e6102c87d824ccee82fb51bd95bad5ab0135
ineedsomeweed <e48728@gmail.com>
2023-12-07 15:55:17 +0800

    20231207

---
+++ 10_7/10_7.cpp
@@ -0,0 +1,36 @@
+﻿// 10_7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+struct card {
+
+    const char *face;
+    const char *suit;
+};
+
+int main(void)
+{
+    struct card aCard;
+    struct card *cardPtr;
+
+    aCard.face = "Ace";
+    aCard.suit = "Spades";
+
+    cardPtr = &aCard;
+    printf("%s%s%s\n%s%s%s\n%s%s%s\n", aCard.face, " of ", aCard.suit, cardPtr->face, " of ", cardPtr->suit, (*cardPtr).face, " of ", (*cardPtr).suit);
+    return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
